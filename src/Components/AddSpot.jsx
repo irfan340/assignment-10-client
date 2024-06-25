5c7b4a551304827c190254c329a5af907de064a6
irfan340 <61534088+irfan340@users.noreply.github.com.>
2024-06-25 10:34:27 +0600

    added name and style in name and quantity form

--- src/Components/AddSpot.jsx
+++ src/Components/AddSpot.jsx
@@ -2,20 +2,22 @@
 
 const AddSpot = () => {
     return (
-        <div>
+        <div className="bg-slate-400 p-24">
+            <h2 className="text-3xl font-extrabold">Add Spot</h2>
+            {/* form name and quantity */}
             <form>
-                <div className="flex">
-                    <label className="form-control w-full max-w-xs">
+                <div className="flex gap-4">
+                    <label className="form-control w-full">
                         <div className="label">
                             <span className="label-text">Name</span>
                         </div>
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input name='name' type="text" placeholder="Type here" className="input input-bordered w-full" />
                     </label>
-                    <label className="form-control w-full max-w-xs">
+                    <label className="form-control w-full">
                         <div className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text">Quantity</span>
                         </div>
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input name="quantity" type="text" placeholder="Available Quantity" className="input input-bordered w-full" />
                     </label>
                 </div>
             </form>
