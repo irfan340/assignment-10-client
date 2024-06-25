d27a0477f91367917e1713cebcb1682e6f1ab716
irfan340 <61534088+irfan340@users.noreply.github.com.>
2024-06-25 12:13:10 +0600

    solved the problem of form

--- src/Components/AddSpot.jsx
+++ src/Components/AddSpot.jsx
@@ -1,39 +1,55 @@
 
 
 const AddSpot = () => {
+    let handleAddSpot = e => {
+        e.preventDefault()
+        let form = e.target
+
+        let name = form.name.value
+        let location = form.location.value
+        let photo = form.photo.value
+
+        let newSpot = { name, location, photo }
+        console.log(newSpot)
+    }
     return (
         <div className="bg-slate-400 p-24">
             <h2 className="text-3xl font-extrabold">Add Spot</h2>
-            {/*name and quantity form*/}
-            <form>
-                <div className="flex gap-4">
-                    <label className="form-control w-full">
-                        <div className="label">
-                            <span className="label-text">Name</span>
-                        </div>
-                        <input name='name' type="text" placeholder="Type here" className="input input-bordered w-full" />
-                    </label>
-                    <label className="form-control w-full">
-                        <div className="label">
-                            <span className="label-text">Quantity</span>
-                        </div>
-                        <input name="quantity" type="text" placeholder="Available Quantity" className="input input-bordered w-full" />
-                    </label>
+            <form onSubmit={handleAddSpot}>
+                {/*name and quantity form*/}
+                <div className="md:flex">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Spot Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" placeholder="Coffee Name" name="name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 md:ml-4">
+                        <label className="label">
+                            <span className="label-text">Location</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="location" placeholder="Available Quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
                 </div>
-            </form>
-            {/* Photo URL form */}
-            <form>
-                <div className="flex gap-4">
-                    <label className="form-control w-full">
-                        <div className="label">
-                            <span className="label-text">Name</span>
-                        </div>
-                        <input name='name' type="text" placeholder="Type here" className="input input-bordered w-full" />
-                    </label>
+                {/* Photo URL form */}
+                <div className="mb-8">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" placeholder="Photo URL" name="photo" className="input input-bordered w-full" />
+                        </label>
+                    </div>
                 </div>
+                {/* <input type="submit" value="Add Coffee" className="btn btn-block bg-gray-900 text-white"/> */}
+                {/* button */}
+                <input type="submit" value="Add Spot" className="btn btn-block mt-2" />
             </form>
-            {/* button */}
-            <input type="Submit" value="Add Spot" className="btn btn-block mt-2"/>
         </div>
     );
 };
