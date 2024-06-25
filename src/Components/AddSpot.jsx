f7b7461f218aff981d01dd15db57ceba1580f5d0
irfan340 <61534088+irfan340@users.noreply.github.com.>
2024-06-25 10:50:34 +0600

    create a submit type input field and added className & value

--- src/Components/AddSpot.jsx
+++ src/Components/AddSpot.jsx
@@ -4,7 +4,7 @@ const AddSpot = () => {
     return (
         <div className="bg-slate-400 p-24">
             <h2 className="text-3xl font-extrabold">Add Spot</h2>
-            {/* form name and quantity */}
+            {/*name and quantity form*/}
             <form>
                 <div className="flex gap-4">
                     <label className="form-control w-full">
@@ -21,6 +21,19 @@ const AddSpot = () => {
                     </label>
                 </div>
             </form>
+            {/* Photo URL form */}
+            <form>
+                <div className="flex gap-4">
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text">Name</span>
+                        </div>
+                        <input name='name' type="text" placeholder="Type here" className="input input-bordered w-full" />
+                    </label>
+                </div>
+            </form>
+            {/* button */}
+            <input type="Submit" value="Add Spot" className="btn btn-block mt-2"/>
         </div>
     );
 };
